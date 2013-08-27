914012a5363d2c9d7ba2af0a6a032af1211d7524
Karsten Wolf <karstenwo@web.de>
2013-08-27 13:13:01 +0200

    Create new release shell script.

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -Rf ./build/*
+rm -Rf ./dist/*
+
+/usr/bin/env python setup.py py2app
+
+mkdir -p Launcher_v000
+rm -Rf ./Launcher_v000/Launcher.app
+
+cp -p ./LICENSE ./Launcher_v000/
+cp -p ./README.md ./Launcher_v000/
+
+mv ./dist/Launcher.app ./Launcher_v000/
+
+# markdown README.md >README.html
+markdown_py -o html5 -f README.html README.md
+cp README.html  ./Launcher_v000/
+
+
+
