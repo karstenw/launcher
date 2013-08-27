9e4037f0735c92b061d889be58772f70b679f290
Karsten Wolf <karstenwo@web.de>
2013-08-27 15:48:23 +0200

    Initial.

---
+++ LauncherVersion.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+year = datetime.datetime.now().year
+
+name = 'Launcher'
+version = '0.1.1'
+bundle = 'org.kw.Launcher'
+
+creator = 'KWFL'
+
+shortCopyright = u'Copyright 2008-%i Karsten Wolf' % year
+longCopyright = u'Launcher ' + version + ', Copyright 2008-%i Karsten Wolf' % year
